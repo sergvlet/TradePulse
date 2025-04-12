4125d0f37130267cb9d79c39e813cbae499fbf81
schic <schicu1004@gmail.com>
2025-04-12 15:27:12 +0200

    1

---
+++ src/main/java/com/chicu/tradepulse/TradePulseApplication.java
@@ -0,0 +1,13 @@
+package com.chicu.tradepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradePulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradePulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chicu/tradepulse/TradePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chicu.tradepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradePulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
